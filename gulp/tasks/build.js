122f35b2eb9750740245ec201ace24afad5973f9
Rami Ashqar <rami.g.a_1994@hotmail.com>
2018-05-01 13:27:37 +0300

    Added autopefixer support

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,6 +1,7 @@
-
 var gulp = require('gulp'),
     imagemin = require('gulp-imagemin'),
+    postcss = require('gulp-postcss'),
+    prefixer = require('autoprefixer'),
     del = require('del'),
     usemin = require('gulp-usemin'),
     rev = require('gulp-rev'),
@@ -9,36 +10,53 @@ var gulp = require('gulp'),
     browserSync = require('browser-sync').create();
 
 
-gulp.task('previewDist', function() {
+gulp.task('previewDist', function () {
     browserSync.init({
         notify: false,
         server: {
             baseDir: "docs"
-        }});
+        }
+    });
 });
 
-gulp.task('deleteDistFolder',['icons'], function () {
+
+gulp.task('deleteDistFolder', ['icons'], function () {
     return del('./docs');
 });
 
 gulp.task('usemin', ['deleteDistFolder', 'scripts'], function () {
-   return gulp.src('./app/index.html')
-    .pipe(usemin({
-        css: [
-            function(){return rev();},
-            function(){return  cssnano();}
-        ],
-        js: [
-            function(){return rev();},
-            function(){return  uglify();}
-        ]
-    }))
-    .pipe(gulp.dest('./docs/'));
+    return gulp.src('./app/index.html')
+        .pipe(usemin({
+            css: [
+                function () {
+                    return rev();
+                },
+                function () {
+                    return cssnano();
+                }
+            ],
+            js: [
+                function () {
+                    return rev();
+                },
+                function () {
+                    return uglify();
+                }
+            ]
+        }))
+        .pipe(gulp.dest('./docs/'));
+});
+gulp.task('prefix', ['usemin'], function() {
+    return gulp.src('./docs/assets/styles/**/*.css')
+        .pipe(postcss([
+            prefixer ()
+        ]))
+      .pipe(gulp.dest('./docs/assets/styles'));
 });
 
 gulp.task('optimizeImages', ['deleteDistFolder'], function () {
     var imageSrc = ['./app/assets/images/**/*', '!./app/assets/images/icons', '!./app/assets/images/icons/**/*',
-                    '!./app/assets/images/iImages', '!./app/assets/images/iImages/**/*'                
+        '!./app/assets/images/iImages', '!./app/assets/images/iImages/**/*'
     ];
     return gulp.src(imageSrc)
         .pipe(imagemin({
@@ -48,14 +66,14 @@ gulp.task('optimizeImages', ['deleteDistFolder'], function () {
         }))
         .pipe(gulp.dest('./docs/assets/images/'));
 });
-gulp.task('copyGeneralFiles', ['deleteDistFolder'], function(){
-    var generalSrc= [
-        './app/assets/**/*','!./app/assets/images', '!./app/assets/styles', '!./app/assets/scripts',
+gulp.task('copyGeneralFiles', ['deleteDistFolder'], function () {
+    var generalSrc = [
+        './app/assets/**/*', '!./app/assets/images', '!./app/assets/styles', '!./app/assets/scripts',
         '!./app/assets/images/**/*', '!./app/assets/styles/**/*', '!./app/assets/scripts/**/*'
-    ] ;
+    ];
     return gulp.src(generalSrc)
-      .pipe(gulp.dest('./docs/assets/'));
+        .pipe(gulp.dest('./docs/assets/'));
 });
 
 
-gulp.task('build', ['deleteDistFolder', 'optimizeImages', 'usemin', 'copyGeneralFiles' ]);
+gulp.task('build', [ 'deleteDistFolder', 'optimizeImages', 'usemin','prefix', 'copyGeneralFiles']);
